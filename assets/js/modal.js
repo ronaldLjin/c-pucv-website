d2eea92fd3b47933873715a24bcffebfdd7ca7e1
ronaldLjin <ronyjin123@gmail.com>
2022-07-22 16:21:23 -0600

    test

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,17 +3,17 @@ let displayModal = function(e) {
     let mask =  document.getElementById("mask")
     mask.style.display = "block"
     modal.style.display = "block"
-    modal.classList.remove("animate__animated", "animate__slideOutDown", "animate__faster")
-    modal.classList.add("animate__animated", "animate__slideInUp", "animate__faster")
-    mask.classList.remove("animate__animated", "animate__fadeOut", "animate__faster")
-    mask.classList.add("animate__animated", "animate__fadeIn", "animate__faster")
+    // modal.classList.remove("animate__animated", "animate__slideOutDown", "animate__faster")
+    // modal.classList.add("animate__animated", "animate__slideInUp", "animate__faster")
+    // mask.classList.remove("animate__animated", "animate__fadeOut", "animate__faster")
+    // mask.classList.add("animate__animated", "animate__fadeIn", "animate__faster")
 
     let removeModal = function(element) {
         element.addEventListener('click', () => {
-            modal.classList.remove("animate__animated", "animate__slideInUp", "animate__faster")
-            modal.classList.add("animate__animated", "animate__slideOutDown", "animate__faster")
-            mask.classList.remove("animate__animated", "animate__fadeIn", "animate__faster")
-            mask.classList.add("animate__animated", "animate__fadeOut", "animate__faster")
+            // modal.classList.remove("animate__animated", "animate__slideInUp", "animate__faster")
+            // modal.classList.add("animate__animated", "animate__slideOutDown", "animate__faster")
+            // mask.classList.remove("animate__animated", "animate__fadeIn", "animate__faster")
+            // mask.classList.add("animate__animated", "animate__fadeOut", "animate__faster")
             setTimeout(function() {
                 mask.style.display = "none"
                 modal.style.display = "none"
