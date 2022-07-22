0a137336f353d4a23d86672eb77b00b181f7df0b
ronaldLjin <ronyjin123@gmail.com>
2022-07-22 15:56:11 -0600

    Video poster

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -6,8 +6,10 @@ for (i = 0; i < acc.length; i++) {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
+      panel.style.padding = "0 15px"
       panel.style.maxHeight = null;
     } else {
+      panel.style.padding = "0 15px 15px 15px"
       panel.style.maxHeight = panel.scrollHeight + "px";
       panel.style.overflowY = "auto"
     }
--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,11 +3,21 @@ let displayModal = function(e) {
     let mask =  document.getElementById("mask")
     mask.style.display = "block"
     modal.style.display = "block"
+    modal.classList.remove("animate__animated", "animate__slideOutDown", "animate__faster")
+    modal.classList.add("animate__animated", "animate__slideInUp", "animate__faster")
+    mask.classList.remove("animate__animated", "animate__fadeOut", "animate__faster")
+    mask.classList.add("animate__animated", "animate__fadeIn", "animate__faster")
 
     let removeModal = function(element) {
         element.addEventListener('click', () => {
-            mask.style.display = "none"
-            modal.style.display = "none"
+            modal.classList.remove("animate__animated", "animate__slideInUp", "animate__faster")
+            modal.classList.add("animate__animated", "animate__slideOutDown", "animate__faster")
+            mask.classList.remove("animate__animated", "animate__fadeIn", "animate__faster")
+            mask.classList.add("animate__animated", "animate__fadeOut", "animate__faster")
+            setTimeout(function() {
+                mask.style.display = "none"
+                modal.style.display = "none"
+            }, 500)
         })
     }  
     
@@ -18,3 +28,6 @@ let displayModal = function(e) {
 
 const volunteer = document.getElementById("volunteer")
 volunteer.addEventListener("click", displayModal)
+
+const partner = document.getElementById("partner")
+partner.addEventListener("click", displayModal)
