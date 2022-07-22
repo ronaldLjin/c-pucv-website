81a91917dfe7f0f7adb9c3dd1c31168ba6d53c94
ronaldLjin <ronyjin123@gmail.com>
2022-07-22 16:25:45 -0600

    testing

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,33 +1,37 @@
-let displayModal = function(e) {
-    let modal = document.getElementById(`${e.path[0].id}-modal`)
-    let mask =  document.getElementById("mask")
+let displayModal = function (id) {
+    let modal = document.getElementById(`${id}-modal`)
+    let mask = document.getElementById("mask")
     mask.style.display = "block"
     modal.style.display = "block"
-    // modal.classList.remove("animate__animated", "animate__slideOutDown", "animate__faster")
-    // modal.classList.add("animate__animated", "animate__slideInUp", "animate__faster")
-    // mask.classList.remove("animate__animated", "animate__fadeOut", "animate__faster")
-    // mask.classList.add("animate__animated", "animate__fadeIn", "animate__faster")
+    modal.classList.remove("animate__animated", "animate__slideOutDown", "animate__faster")
+    modal.classList.add("animate__animated", "animate__slideInUp", "animate__faster")
+    mask.classList.remove("animate__animated", "animate__fadeOut", "animate__faster")
+    mask.classList.add("animate__animated", "animate__fadeIn", "animate__faster")
 
-    let removeModal = function(element) {
+    let removeModal = function (element) {
         element.addEventListener('click', () => {
-            // modal.classList.remove("animate__animated", "animate__slideInUp", "animate__faster")
-            // modal.classList.add("animate__animated", "animate__slideOutDown", "animate__faster")
-            // mask.classList.remove("animate__animated", "animate__fadeIn", "animate__faster")
-            // mask.classList.add("animate__animated", "animate__fadeOut", "animate__faster")
-            setTimeout(function() {
+            modal.classList.remove("animate__animated", "animate__slideInUp", "animate__faster")
+            modal.classList.add("animate__animated", "animate__slideOutDown", "animate__faster")
+            mask.classList.remove("animate__animated", "animate__fadeIn", "animate__faster")
+            mask.classList.add("animate__animated", "animate__fadeOut", "animate__faster")
+            setTimeout(function () {
                 mask.style.display = "none"
                 modal.style.display = "none"
             }, 500)
         })
-    }  
-    
+    }
+
     let closeButtons = document.querySelectorAll(".close")
     closeButtons.forEach(removeModal)
-    removeModal(mask)  
+    removeModal(mask)
 }
 
 const volunteer = document.getElementById("volunteer")
-volunteer.addEventListener("click", displayModal)
+volunteer.addEventListener("click", () => {
+    displayModal("volunteer")
+})
 
 const partner = document.getElementById("partner")
-partner.addEventListener("click", displayModal)
+partner.addEventListener("click", () => {
+    displayModal("partner")
+})
