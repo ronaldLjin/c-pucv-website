ec59bd4482cf91bccc1882379b488a78f0bb351a
ronaldLjin <ronyjin123@gmail.com>
2022-07-24 11:58:50 -0600

    tidying

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,15 +1,15 @@
 const swiper1 = new Swiper('.swiper1', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: "auto",
-    spaceBetween: 30,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  // Optional parameters
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 30,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
 const swiper2 = new Swiper('.swiper2', {
   // Optional parameters
