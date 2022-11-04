6d73857e5b6198fe0d3fd109884ca92b9bd3be37
ronaldLjin <ronyjin123@gmail.com>
2022-11-03 23:25:49 -0400

    Updated date and location

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
-// let eventDate = new Date("Apr 18, 2023 12:00:00").getTime();
+let eventDate = new Date("Jan 24, 2023 12:00:00").getTime();
 
-// let x = setInterval(function () {
+let x = setInterval(function () {
 
-//     let now = new Date().getTime();
+    let now = new Date().getTime();
 
-//     let distance = eventDate - now;
+    let distance = eventDate - now;
 
-//     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-//     if (distance > 0) {
-//         document.getElementById("days").innerHTML = days
-//         document.getElementById("hours").innerHTML = hours
-//         document.getElementById("minutes").innerHTML = minutes
-//         document.getElementById("seconds").innerHTML = seconds
-//     }
-//     else if (distance < 0 && distance > -21600000) {
-//         document.getElementById("countdown").innerHTML = "Come drop by, We are open!";
-//     }
-//     else if (distance < -21600000) {
-//         clearInterval(x);
-//         document.getElementById("countdown").textContent = "The event is over. See you next year!";
-//     }
-// });
+    if (distance > 0) {
+        document.getElementById("days").innerHTML = days
+        document.getElementById("hours").innerHTML = hours
+        document.getElementById("minutes").innerHTML = minutes
+        document.getElementById("seconds").innerHTML = seconds
+    }
+    else if (distance < 0 && distance > -21600000) {
+        document.getElementById("countdown").innerHTML = "Come drop by, We are open!";
+    }
+    else if (distance < -21600000) {
+        clearInterval(x);
+        document.getElementById("countdown").textContent = "The event is over. See you next year!";
+    }
+});
